1a2fa1a3eca426e69b6ffce622ba458557403a8b
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-11 20:26:12 -0300

    :tada: Initial commit

---
+++ components/AppButton.vue
@@ -0,0 +1,448 @@
+<template>
+    <button class="main-button">
+        <span class="hover top one"/>
+        <span class="hover top two"/>
+        <span class="hover top three"/>
+        <span class="hover top four"/>
+        <span class="hover top five"/>
+        <span class="hover top six"/>
+        <span class="hover top seven"/>
+        <span class="hover top eight"/>
+        <span class="hover top-center one"/>
+        <span class="hover top-center two"/>
+        <span class="hover top-center three"/>
+        <span class="hover top-center four"/>
+        <span class="hover top-center five"/>
+        <span class="hover top-center six"/>
+        <span class="hover top-center seven"/>
+        <span class="hover top-center eight"/>
+        <span class="hover center one"/>
+        <span class="hover center two"/>
+        <span class="hover center three"/>
+        <span class="hover center four"/>
+        <span class="hover center five"/>
+        <span class="hover center six"/>
+        <span class="hover center seven"/>
+        <span class="hover center eight"/>
+        <span class="hover bottom-center one"/>
+        <span class="hover bottom-center two"/>
+        <span class="hover bottom-center three"/>
+        <span class="hover bottom-center four"/>
+        <span class="hover bottom-center five"/>
+        <span class="hover bottom-center six"/>
+        <span class="hover bottom-center seven"/>
+        <span class="hover bottom-center eight"/>
+        <span class="hover bottom one"/>
+        <span class="hover bottom two"/>
+        <span class="hover bottom three"/>
+        <span class="hover bottom four"/>
+        <span class="hover bottom five"/>
+        <span class="hover bottom six"/>
+        <span class="hover bottom seven"/>
+        <span class="hover bottom eight"/>
+        
+        <span class="background">
+            {{ text }}
+        </span>
+        
+        <span class="glass">
+            {{ text }}
+        </span>
+
+    </button>
+</template>
+
+<script setup>
+import { toRefs } from '#imports'
+
+const props = defineProps({
+    text: {
+        type: String,
+        default: 'Missing Text'
+    }
+})
+
+const {  } = toRefs(props)
+</script>
+
+<style lang="scss" scoped>
+.main-button {
+    outline: transparent;
+    position: relative;
+    border: none;
+    background-color: transparent;
+
+    .background {
+        display: block;
+        background-color: $gray;
+        padding: 12px 28px;
+        border-radius: 8px;
+        width: 100%;
+        height: 100%;
+        color: transparent;
+        user-select: none;
+    }
+
+    .hover {
+        position: absolute;
+        width: 12.5%;
+        height: 20%;
+        z-index: 1;
+        cursor: pointer;
+        &:hover {
+            & ~ .glass {
+                background-color: rgba(whitesmoke, 0.2);
+                background-image: radial-gradient(transparent, rgba(whitesmoke, 0.6)), url('/images/background-noise.png');
+                backdrop-filter: blur(4px);
+                border-color: $pure_white;
+            }
+        }
+        &.top {
+            top: 0;
+            &.one {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(35px, 10px);
+                    }
+                }
+            }
+            &.two {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(25px, 10px);
+                    }
+                }
+            }
+            &.three {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(15px, 10px);
+                    }
+                }
+            }
+            &.four {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(5px, 10px);
+                    }
+                }
+            }
+            &.five {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-5px, 10px);
+                    }
+                }
+            }
+            &.six {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-15px, 10px);
+                    }
+                }
+            }
+            &.seven {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-25px, 10px);
+                    }
+                }
+            }
+            &.eight {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-35px, 10px);
+                    }
+                }
+            }
+            &-center {
+                &.one {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(35px, 12px);
+                        }
+                    }
+                }
+                &.two {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(25px, 12px);
+                        }
+                    }
+                }
+                &.three {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(15px, 12px);
+                        }
+                    }
+                }
+                &.four {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(5px, 12px);
+                        }
+                    }
+                }
+                &.five {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-5px, 12px);
+                        }
+                    }
+                }
+                &.six {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-15px, 12px);
+                        }
+                    }
+                }
+                &.seven {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-25px, 12px);
+                        }
+                    }
+                }
+                &.eight {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-35px, 12px);
+                        }
+                    }
+                }
+            }
+        }
+        &.top-center {
+            top: 20%;
+        }
+        &.center {
+            top: 40%;
+            &.one {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(35px, 14px);
+                    }
+                }
+            }
+            &.two {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(25px, 14px);
+                    }
+                }
+            }
+            &.three {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(15px, 14px);
+                    }
+                }
+            }
+            &.four {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(5px, 14px);
+                    }
+                }
+            }
+            &.five {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-5px, 14px);
+                    }
+                }
+            }
+            &.six {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-15px, 14px);
+                    }
+                }
+            }
+            &.seven {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-25px, 14px);
+                    }
+                }
+            }
+            &.eight {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-35px, 14px);
+                    }
+                }
+            }
+        }
+        &.bottom-center {
+            top: 60%;
+        }
+        &.bottom {
+            top: 80%;
+            &.one {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(35px, 18px);
+                    }
+                }
+            }
+            &.two {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(25px, 18px);
+                    }
+                }
+            }
+            &.three {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(15px, 18px);
+                    }
+                }
+            }
+            &.four {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(5px, 18px);
+                    }
+                }
+            }
+            &.five {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-5px, 18px);
+                    }
+                }
+            }
+            &.six {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-15px, 18px);
+                    }
+                }
+            }
+            &.seven {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-25px, 18px);
+                    }
+                }
+            }
+            &.eight {
+                &:hover {
+                    & ~ .background {
+                        transform: translate(-35px, 18px);
+                    }
+                }
+            }
+            &-center {
+                &.one {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(35px, 16px);
+                        }
+                    }
+                }
+                &.two {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(25px, 16px);
+                        }
+                    }
+                }
+                &.three {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(15px, 16px);
+                        }
+                    }
+                }
+                &.four {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(5px, 16px);
+                        }
+                    }
+                }
+                &.five {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-5px, 16px);
+                        }
+                    }
+                }
+                &.six {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-15px, 16px);
+                        }
+                    }
+                }
+                &.seven {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-25px, 16px);
+                        }
+                    }
+                }
+                &.eight {
+                    &:hover {
+                        & ~ .background {
+                            transform: translate(-35px, 16px);
+                        }
+                    }
+                }
+            }
+        }
+        &.one {
+            left: 0;
+        }
+        &.two {
+            left: 12.5%;
+        }
+        &.three {
+            left: 25%;
+        }
+        &.four {
+            left: 37.5%;
+        }
+        &.five {
+            left: 50%;
+        }
+        &.six {
+            left: 62.5%;
+        }
+        &.seven {
+            left: 75%;
+        }
+        &.eight {
+            left: 87.5%;
+        }
+    }
+
+    .glass {
+        position: absolute;
+        inset: 0;
+        border-radius: 8px;
+        background-color: transparent;
+        backdrop-filter: blur(0);
+        display: grid;
+        place-items: center;
+        white-space: nowrap;
+        font-size: 18px;
+        font-weight: 500;
+        border: 1px solid transparent;
+        transition: all 0.3s ease;
+    }
+
+
+    &.primary {
+        .background {
+            background-color: $light_blue;
+            transition: all 0.3s ease;
+        }
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="wrapper">
+        <BlobBackground/>
+        <main class="content">
+            <section class="hero">
+                <h1 class="title">
+                    Groovy Beats for Relaxing, Working,<br/> and Jamming While Studying
+                </h1>
+                <p class="subheading">
+                    Elevate your Listening experience: join us on the radio for an eclectic mix of groovy beats, perfectly suited to enhance relaxation, boost productivity, and infuse your study sessions with a fun and lively rhythm!
+                </p>
+                <AppButton class="primary" text="Start listening"/>
+            </section>
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+    height: 100%;
+    background-color: $background_color;
+
+    .content {
+        position: relative;
+        z-index: 2;
+        max-width: 1300px;
+        margin-inline: auto;
+
+        .hero {
+            height: 100vh;
+            height: 100dvh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            color: $text_color;
+            
+            .title {
+                text-align: center;
+                font-size: 60px;
+                position: relative;
+                background-image: $title_gradient;
+                color: transparent;
+                background-clip: text;
+            }
+
+            .subheading {
+                max-width: 700px;
+                text-align: center;
+                margin-block: 20px 32px;
+                font-size: 18px;
+            }
+        }
+    }
+
+}
+</style>
