92a39929bdffc57a4dd52849562f5e175c8ee910
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-13 15:15:20 -0300

    :lipstick: Sidebar player styling

--- components/PlayerPage.vue
+++ components/PlayerPage.vue
@@ -23,7 +23,7 @@
         </div>
         <aside class="player-sidebar">
             <h2 class="title">
-                Now playing
+                Next on your playlist
             </h2>
             <ul class="playlist">
                 <li 
@@ -31,10 +31,10 @@
                     :key="music.id"
                     class="music"
                 >
-                    <img :src="music.picture" alt=""/>
+                    <img :src="music.picture" alt="" class="music-cover"/>
                     <span class="music-text">
-                        <strong>{{ music.name }}</strong>
-                        <small>{{ music.artist }}</small>
+                        <strong class="music-text-title">{{ music.name }}</strong>
+                        <small class="music-text-artist">{{ music.artist }}</small>
                     </span>
                     <button class="music-actions">
                         <span class="visually-hidden">Actions for {{ music.name }}</span>
@@ -42,6 +42,37 @@
                     </button>
                 </li>
             </ul>
+            <div class="controls">
+                <img :src="playlist[0].picture" alt="" class="controls-cover"/>
+                <span class="controls-content">
+                    <strong class="controls-content-title">{{ playlist[0].name }}</strong>
+                    <small class="controls-content-artist">{{ playlist[0].artist }}</small>
+                    <span class="controls-content-actions">
+                        <span class="controls-content-actions-buttons">
+                            <button>
+                                <span class="visually-hidden">Play previous sound</span>
+                                <Icon name="ph:skip-back-fill"/>
+                            </button>
+                            <button v-if="true">
+                                <span class="visually-hidden">Play</span>
+                                <Icon name="ph:play-fill"/>
+                            </button>
+                            <button v-else>
+                                <span class="visually-hidden">Pause</span>
+                                <Icon name="ph:pause-fill"/>
+                            </button>
+                            <button>
+                                <span class="visually-hidden">Play next sound</span>
+                                <Icon name="ph:skip-forward-fill"/>
+                            </button>
+                        </span>
+                        <label for="track" class="visually-hidden">
+                            Navigate through current sound
+                        </label>
+                        <input id="track" type="range"/>
+                    </span>
+                </span>
+            </div>
         </aside>
     </section>
 </template>
@@ -82,7 +113,79 @@ const playlist = reactive([
         name: 'AMERICAN HORROR SHOW',
         artist: 'SNOW WIFE',
         picture: 'https://picsum.photos/seed/music_4/200'
-    }
+    },
+    {
+        id: 'music_5',
+        name: 'Freak Show',
+        artist: 'Jake Daniels, AViVA',
+        picture: 'https://picsum.photos/seed/music_1/200'
+    },
+    {
+        id: 'music_6',
+        name: 'Bad Side',
+        artist: 'Jake Daniels',
+        picture: 'https://picsum.photos/seed/music_2/200'
+    },
+    {
+        id: 'music_7',
+        name: 'BAD BITCH',
+        artist: 'Tessa Violet',
+        picture: 'https://picsum.photos/seed/music_3/200'
+    },
+    {
+        id: 'music_8',
+        name: 'AMERICAN HORROR SHOW',
+        artist: 'SNOW WIFE',
+        picture: 'https://picsum.photos/seed/music_4/200'
+    },
+    {
+        id: 'music_9',
+        name: 'Freak Show',
+        artist: 'Jake Daniels, AViVA',
+        picture: 'https://picsum.photos/seed/music_1/200'
+    },
+    {
+        id: 'music_10',
+        name: 'Bad Side',
+        artist: 'Jake Daniels',
+        picture: 'https://picsum.photos/seed/music_2/200'
+    },
+    {
+        id: 'music_11',
+        name: 'BAD BITCH',
+        artist: 'Tessa Violet',
+        picture: 'https://picsum.photos/seed/music_3/200'
+    },
+    {
+        id: 'music_12',
+        name: 'AMERICAN HORROR SHOW',
+        artist: 'SNOW WIFE',
+        picture: 'https://picsum.photos/seed/music_4/200'
+    },
+    {
+        id: 'music_13',
+        name: 'Freak Show',
+        artist: 'Jake Daniels, AViVA',
+        picture: 'https://picsum.photos/seed/music_1/200'
+    },
+    {
+        id: 'music_14',
+        name: 'Bad Side',
+        artist: 'Jake Daniels',
+        picture: 'https://picsum.photos/seed/music_2/200'
+    },
+    {
+        id: 'music_15',
+        name: 'BAD BITCH',
+        artist: 'Tessa Violet',
+        picture: 'https://picsum.photos/seed/music_3/200'
+    },
+    {
+        id: 'music_16',
+        name: 'AMERICAN HORROR SHOW',
+        artist: 'SNOW WIFE',
+        picture: 'https://picsum.photos/seed/music_4/200'
+    },
 ])
 </script>
 
@@ -138,9 +241,108 @@ const playlist = reactive([
             z-index: 2;
             background-color: lighten($background_color, 2%);
             padding: 20px 80px;
+            display: flex;
+            flex-direction: column;
             .title {
-                font-weight: 500;
-                font-size: 18px;
+                font-weight: 400;
+                font-size: 16px;
+            }
+            .playlist {
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+                flex-grow: 1;
+                max-height: 100%;
+                overflow: auto;
+                margin-block: 12px 28px;
+                .music {
+                    display: flex;
+                    align-items: center;
+                    &-cover {
+                        max-width: 48px;
+                        border-radius: 4px;
+                    }
+                    &-text {
+                        display: flex;
+                        flex-direction: column;
+                        margin-inline: 8px;
+                        flex-grow: 1;
+                        &-title {
+                            font-weight: 500;
+                            font-size: 14px;
+                        }
+                        &-artist {
+                            font-size: 12px;
+                            white-space: nowrap;
+                            overflow: hidden;
+                            max-width: 16ch;
+                            text-overflow: ellipsis;
+                        }
+                    }
+                    &-actions {
+                        background-color: transparent;
+                        color: $text_color;
+                        font-size: 24px;
+                        padding: 2px;
+                        border: none;
+                        &:focus {
+                            outline: transparent;
+                        }
+                    }
+                }
+            }
+
+            .controls {
+                position: relative;
+                &-cover {
+                    width: 100%;
+                    border-radius: 12px;
+                }
+
+                &-content {
+                    min-width: 94%;
+                    position: absolute;
+                    bottom: 8px;
+                    left: 50%;
+                    padding: 20px 16px;
+                    transform: translateX(-50%);
+                    background-color: rgba($background-color, 0.8);
+                    border-radius: 8px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+
+                    &-title {
+                        font-weight: 500;
+                    }
+
+                    &-artist {
+                        font-size: 12px;
+                        color: rgba($text_color, 0.8);
+                        margin-block: 4px 8px;
+                    }
+
+                    &-actions {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        gap: 4px;
+                        &-buttons {
+                            display: flex;
+                            gap: 12px;
+                            button {
+                                padding: 4px;
+                                border: none;
+                                background-color: transparent;
+                                color: $text_color;
+                                font-size: 24px;
+                                &:focus {
+                                    outline: transparent;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
